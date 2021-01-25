6a4f6e02e8c8414c82473386a83c51d75079ff00
koeonyack <covenant.ko@kakao.com>
2021-01-25 15:22:04 +0900

    add: 15:30
    - css 햄버거 버튼, 로고 위치 버그 수정
    - 검색창 css 추가
    - 비동기로 검색 결과 가져오는 것 추가

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -8,22 +8,39 @@
         <a
           href="javascript:void(0)"
           class="logo"></a>
+        <div class="search">
+          <input 
+            v-model="searchText"
+            placeholder="찾고 싶은 상품을 검색해 보세요!"
+            type="text" 
+            @keyup.enter="search" />
+          <div
+            class="search__icon"
+            @click="search"></div> <!-- BEM -->
+        </div>
       </div>
     </header>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
-
+          searchText: ''
         }
     },
     methods: {
         onNav() {
             // Open LNB
             this.$store.dispatch('navigation/onNav') // navigation파일 안에 onNav를 실행한다. 
+        },
+        async search () { // 비동기동작
+          if (!this.searchText.trim()) return
+          const res = await axios.get(`https://trusting-williams-8cacfb.netlify.app/.netlify/functions/search?apiKey=${1216}&searchText=${this.searchText}`)
+          console.log(res)
         }
     }
 }
@@ -32,6 +49,10 @@ export default {
 <style scoped lang="scss">
   header {
     .inner {
+      display: flex;
+      align-items: center;
+      height: 120px;
+
       .open-nav-drawer {
         display: flex;
         justify-content: center;
@@ -57,11 +78,40 @@ export default {
         display: block;
         width: 94px;
         height: 40px;
-        // margin: 0 24px;
+        margin: 0 24px;
         background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
         background-position: -162px 0;
         background-size: 363px;
         cursor: pointer;
+      }
+      .search {
+        position: relative;
+        input {
+          // display: inline-block;
+          width: 500px;
+          height: 50px;
+          padding: 0 27px;
+          border: 1px solid #ddd;
+          border-radius: 25px;
+          box-sizing: border-box; // padding, border를 500, 50에 맞추어서 
+          background-color: #fafafa;
+          font-size: 18px;
+          outline: none; // input요소에 파란선을 만드는 요인
+          font-family: 'Noto Sans KR', sans-serif;
+        }
+        // & ==> .search
+        // .search__icon
+        &__icon {
+          width: 50px;
+          height: 50px;
+          position: absolute;
+          top: 0;
+          right: 0;
+          cursor: pointer;
+          background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+          background-position: -162px -45px;
+          background-size: 363px;
+        }
       }
     }
   }
