9778a33c1cf5b67dfb02da660514e425fa2ba80c
koeonyack <covenant.ko@kakao.com>
2021-01-26 08:29:59 +0900

    feat: search plugin 전역등록

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import 'regenerator-runtime'
 import Vue from 'vue'
 import store from './store'
 import App from './App'
+import searchData from "./plugins/searchData"
+
+Vue.use(searchData)
 
 new Vue({
     el: '#app',
