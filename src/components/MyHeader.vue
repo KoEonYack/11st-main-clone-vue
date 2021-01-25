6c436c890c51aad3ba88ea79b754687ea1da6da7
koeonyack <covenant.ko@kakao.com>
2021-01-26 08:18:12 +0900

    feat: 11st logo, menu button
    - menu 클릭시 text message 반응

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <header>
-      <div
-        class="open-nav-drawer"
-        @click="onNav">
-        onNav
+      <div class="inner">
+        <div
+          class="open-nav-drawer"
+          @click="onNav"></div>
+        <a
+          href="javascript:void(0)"
+          class="logo"></a>
       </div>
     </header>
   </div>
 </template>
-
 
 <script>
 export default {
@@ -21,7 +23,46 @@ export default {
     methods: {
         onNav() {
             // Open LNB
+            this.$store.dispatch('navigation/onNav') // navigation파일 안에 onNav를 실행한다. 
         }
     }
 }
 </script>
+
+<style scoped lang="scss">
+  header {
+    .inner {
+      .open-nav-drawer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        cursor: pointer;
+        box-shadow: 
+          0 2px 6px rgba(#000, .06),
+          0 0 1px rgba(#000, .4);
+        &::after {
+          content: "";
+          display: block;
+          width: 36px;
+          height: 36px;
+          background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+          background-position: -302px -203px;
+          background-size: 363px;
+        }
+      }
+      .logo {
+        display: block;
+        width: 94px;
+        height: 40px;
+        // margin: 0 24px;
+        background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+        background-position: -162px 0;
+        background-size: 363px;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import store from './store'
 import App from './App'
 
 new Vue({
     el: '#app',
+    store,
     render: createElement => createElement(App)
 })
