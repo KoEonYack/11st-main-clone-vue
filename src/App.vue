b1cd3803636be130c7917403ba56a4211c64813d
koeonyack <covenant.ko@kakao.com>
2021-01-26 16:20:45 +0900

    feat: 상단 슬라이드바 추가

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <l-n-b></l-n-b>
     <my-header></my-header>
+    <billboard></billboard>
   </div>
 </template>
 
 <script>
 import LNB from '~/src/components/LNB'
 import MyHeader from '~/src/components/MyHeader'
+import Billboard from '~/src/components/Billboard'
 
 export default {
   components: {
     LNB,
-    MyHeader
+    MyHeader,
+    Billboard
   }
 }
 </script>
