2f0c31b38f39ccbb109cf071d66c69063d5d0831
koeonyack <covenant.ko@kakao.com>
2021-01-27 15:23:37 +0900

    feat:change netlify url

--- src/plugins/fetchData.js
+++ src/plugins/fetchData.js
@@ -5,7 +5,7 @@ export default {
   install (Vue) {
     Vue.prototype.$fetch = async payload => {
         const { requestName } = payload
-        const { data } = await axios.get(`https://trusting-williams-8cacfb.netlify.app/.netlify/functions/main?apiKey=${process.env.API_KEY}&requestName=${requestName}`)
+        const { data } = await axios.get(`/.netlify/functions/main?apiKey=${process.env.API_KEY}&requestName=${requestName}`)
         return data
     }
   }
--- src/plugins/searchData.js
+++ src/plugins/searchData.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default {
   install (Vue) {
     Vue.prototype.$search = async ({ searchText }) => {
-        const { data } = await axios.get(`https://trusting-williams-8cacfb.netlify.app/.netlify/functions/search?apiKey=${process.env.API_KEY}&searchText=${searchText}`)
+        const { data } = await axios.get(`/.netlify/functions/search?apiKey=${process.env.API_KEY}&searchText=${searchText}`)
         return data;
         // const res = await axios.get(`https://trusting-williams-8cacfb.netlify.app/.netlify/functions/search?apiKey=${1216}&searchText=${searchText}`)
         // return res.data;
