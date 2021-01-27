4465e09c188b37cc9497c11f968492a5c8a80043
koeonyack <covenant.ko@kakao.com>
2021-01-27 09:46:21 +0900

    feat: global banner
    - scroll-to 패키지 설치
    - 글로벌 배너 구현

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <l-n-b></l-n-b>
     <my-header></my-header>
     <billboard></billboard>
+    <global-banner></global-banner>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import LNB from '~/src/components/LNB'
 import MyHeader from '~/src/components/MyHeader'
 import Billboard from '~/src/components/Billboard'
+import GlobalBanner from '~/src/components/GlobalBanner'
 
 export default {
   components: {
     LNB,
     MyHeader,
-    Billboard
+    Billboard,
+    GlobalBanner
   }
 }
 </script>
